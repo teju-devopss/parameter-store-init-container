af5704ec2d636fbbf4dae8ff6d473b1e44f031d5
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-08-01 22:01:59 +0530

    updated data

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for param in $PARAMS; do
-  PARAM=$(echo $param | awk -F, '{print $1}')
-  KEY=$(echo $param | awk -F, '{print $2}')
-  VALUE=$(aws ssm get-parameter --name "$NAME" --region us-east-1 --with-decryption --query "Parameter.Value" --output text)
+for item in $PARAMS; do
+  PARAM=$(echo $item | awk -F, '{print $1}')
+  KEY=$(echo $item | awk -F, '{print $2}')
+  VALUE=$(aws ssm get-parameter --name "$PARAM" --region us-east-1 --with-decryption --query "Parameter.Value" --output text)
   echo "export $KEY=\"$VALUE\"" >> /parameters/params
 done
